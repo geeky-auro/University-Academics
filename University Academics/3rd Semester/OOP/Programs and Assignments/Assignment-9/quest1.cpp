6f1069e564bebc65b2baf1ff50ae30901c3f3122
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-11-20 12:54:34 +0530

    Assignment 9 Completed

--- University Academics/3rd Semester/OOP/Programs and Assignments/Assignment-9/quest1.cpp
+++ University Academics/3rd Semester/OOP/Programs and Assignments/Assignment-9/quest1.cpp
@@ -1,59 +1,72 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class shape
 {
 private:
 public:
-virtual inline double area(){
-    return 0;
-}
+    virtual inline double area()
+    {
+        return 0;
+    }
 };
 
-class circle:public shape{
+class circle : public shape
+{
 private:
     int r;
+
 public:
-    circle(){
-        cout<<"Enter Radius of the Circle"<<endl;
-        cin>>r;
+    circle()
+    {
+        cout << "Enter Radius of the Circle" << endl;
+        cin >> r;
     }
-    inline double area(){
-        return 3.14*r*r;
+    inline double area()
+    {
+        return 3.14 * r * r;
     }
 };
 
-class square:public shape{
+class square : public shape
+{
 private:
-int b;
+    int b;
+
 public:
-square(){
-    cout<<"Enter Breadth of the Square"<<endl;
-    cin>>b;
-}
-inline double area(){
-        return b*b;
+    square()
+    {
+        cout << "Enter Breadth of the Square" << endl;
+        cin >> b;
+    }
+    inline double area()
+    {
+        return b * b;
     }
 };
 
-class triangle: public shape{
+class triangle : public shape
+{
 private:
-int h,b;
+    int h, b;
+
 public:
-triangle(){
-    cout<<"Enter Height and Breadth of the Triangle"<<endl;
-    cin>>h>>b;
-}
-inline double area(){
-    return 0.5*h*b;
-}
+    triangle()
+    {
+        cout << "Enter Height and Breadth of the Triangle" << endl;
+        cin >> h >> b;
+    }
+    inline double area()
+    {
+        return 0.5 * h * b;
+    }
 };
 int main()
 {
     square s;
-    cout<<"Area of the Square "<<s.area()<<endl;
+    cout << "Area of the Square " << s.area() << endl;
     triangle t;
-    cout<<"Area of the Triangle "<<t.area()<<endl;
+    cout << "Area of the Triangle " << t.area() << endl;
     circle c;
-    cout<<"Area of the Circle "<<c.area()<<endl;
+    cout << "Area of the Circle " << c.area() << endl;
     return 0;
 }
